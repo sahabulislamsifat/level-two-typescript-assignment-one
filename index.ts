e2c49d5446c427e7eebf22d3bb71ea6b5ed745bc
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-08 22:50:46 +0600

    problem two solved

--- index.ts
+++ index.ts
@@ -8,3 +8,46 @@ function formatString(input: string, toUpper?: boolean): string {
 formatString("Hello");
 formatString("Hello", true);
 formatString("Hello", false);
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const res1 = items.filter((e) => e.rating >= 4);
+  return res1;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+filterByRating(books);
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo(): void {
+    console.log(`Make: ${this.make}, Year: ${this.year}`);
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel(): void {
+    console.log(`Model: ${this.model}`);
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();
+myCar.getModel();
